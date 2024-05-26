9b56d6176bebffba31871f729fc4226c2631fa5f
ChrisTho23 <christophe.thomassin23@gmail.com>
2024-05-26 16:58:14 -0700

    Add table of contents

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,3 +1,4 @@
+// app/blog/[slug]/page.tsx
 import { notFound } from "next/navigation";
 import { allBlogs } from "contentlayer/generated";
 import { Mdx } from "@/app/components/mdx";
@@ -5,6 +6,7 @@ import { Header } from "./header";
 import "./mdx.css";
 import { ReportView } from "./view"; 
 import { Redis } from "@upstash/redis";
+import { TableOfContents } from "@/app/components/toc"; // Import the TOC component
 
 export const revalidate = 60;
 
@@ -37,8 +39,10 @@ export default async function PostPage({ params }: Props) {
 
   return (
     <div className="bg-zinc-50 min-h-screen">
-      <Header blog={blog} views={views} /> {/* Ensure the Header component is adapted for blogs */}
-      <ReportView slug={blog.slug} /> {/* Ensure this component is adapted for blogs if needed */}
+      <Header blog={blog} views={views} /> 
+      <ReportView slug={blog.slug} /> 
+      
+      <TableOfContents toc={blog.toc} />
 
       <article className="px-4 py-12 mx-auto prose prose-zinc prose-quoteless max-w-3xl">
         <Mdx code={blog.body.code} />
