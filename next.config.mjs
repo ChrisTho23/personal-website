8bb872072672cfb792b43b22cdb5b8b2e5877a82
ChrisTho23 <christophe.thomassin23@gmail.com>
2024-05-26 16:51:20 -0700

    Include remark and rehype plugins in MDX config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,29 @@
 import { withContentlayer } from "next-contentlayer";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 	experimental: {
 		mdxRs: true,
+	},
+	// Add MDX configuration
+	webpack: (config, { defaultLoaders }) => {
+		config.module.rules.push({
+			test: /\.mdx?$/,
+			use: [
+				defaultLoaders.babel,
+				{
+					loader: '@mdx-js/loader',
+					options: {
+						remarkPlugins: [remarkMath],
+						rehypePlugins: [rehypeKatex],
+					},
+				},
+			],
+		});
+		return config;
 	},
 };
 
