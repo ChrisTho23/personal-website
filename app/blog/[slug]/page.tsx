7658d554ab9990999f782d1c7007ce3343ea94d6
ChrisTho23 <110739558+ChrisTho23@users.noreply.github.com>
2024-07-03 10:42:06 -0700

    Update page.tsx (#18)

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -40,7 +40,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     title: blog.title,
     description: blog.description,
     openGraph: {
-      title: blog.title,
+      title: `Blog: ${blog.title}`,
       description: blog.description,
       images: [{ url: imageUrl }],
       url: `https://christophethomassin.com/blog/${blog.slug}`,
@@ -48,7 +48,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     },
     twitter: {
       card: 'summary_large_image',
-      title: blog.title,
+      title: `Blog: ${blog.title}`,
       description: blog.description,
       images: [imageUrl],
     },
